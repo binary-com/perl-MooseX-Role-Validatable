165ebfeb4c1f5bb145e4c15f9edf9f31e739a099
Chylli <chylli.email@gmail.com>
2017-02-27 08:29:04 +0000

    v0.10

--- lib/MooseX/Role/Validatable.pm
+++ lib/MooseX/Role/Validatable.pm
@@ -3,7 +3,7 @@ package MooseX::Role::Validatable;
 use Moose::Role;
 use MooseX::Role::Validatable::Error;
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 use Class::Load qw/load_class/;
 use Carp qw(confess);
